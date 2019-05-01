94981ae46fdef5e086f6ee1817a7bdf1d7583952
Ivens <ivensdiegomuller@gmail.com>
2019-04-30 23:03:39 -0300

    #1 Node API created and listening on port 3000

---
+++ API/src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+
+app.get('/', (req, res) => {
+    res.send('Ok');
+});
+
+app.listen(3000);  
+
+console.log('Node API is running and listening on port 3000');
