603dbba89f41a6b6891746ade3c51ae9538d1fd3
Ivens <ivensdiegomuller@gmail.com>
2019-04-30 23:23:26 -0300

    #2 Vue project created with vuetify

---
+++ InfoStore/infostore/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>    
+    <v-toolbar dark color="#009388">      
+      <v-toolbar-title class="white--text-uppercase">
+        
+        Info Store
+
+      </v-toolbar-title>
+      
+      <v-spacer></v-spacer>          
+    </v-toolbar>
+
+    <v-content>      
+    </v-content>  
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
