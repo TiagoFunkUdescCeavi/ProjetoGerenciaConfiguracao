9415cbe5554e01edff79576ee970b3b03278320a
Ivens <ivensdiegomuller@gmail.com>
2019-05-22 19:29:39 -0300

    #6 Starting product query

--- API/src/controller/productController.js
+++ API/src/controller/productController.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Product = require('../model/product');
+const mongoose = require('../database');
 const router = express.Router();
 
 router.post('/register', async(req, res) => {
@@ -8,6 +9,17 @@ router.post('/register', async(req, res) => {
         return res.send(product);
     }catch(err){
         return res.status(400).send({error: "Product registration failed!"});
+    }
+});
+
+router.get('/', async(req, res) => {
+    try{
+        var products = mongoose.model('Product');
+        products.find(function(err, allProducts){            
+            res.send(allProducts);
+        });
+    }catch(err){
+        return res.status(400).send({error: err});
     }
 });
 
